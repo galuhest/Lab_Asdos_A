fb77c04fc500947ac74d3e63cf2f08ff8c357777
galuhestya <galuhestya@gmail.com>
2015-10-31 20:40:18 +0700

    money -> duit

--- Lab06.java
+++ Lab06.java
@@ -11,27 +11,33 @@ public class Lab06 {
 			if (inputSplit.nextToken().equalsIgnoreCase("Beli")) {
 				String nama = inputSplit.nextToken();
 				farmer.buyAyam(nama);
-				System.out.println(farmer.getNama() + " membeli ayam bernama " + nama);
-				System.out.println("Uang " + farmer.getNama() + " sekarang: " + farmer.getMoney() + " G");
+				System.out.println(farmer.getNama() + " membeli ayam bernama "
+						+ nama);
+				System.out.println("Uang " + farmer.getNama() + " sekarang: "
+						+ farmer.getDuit() + " G");
 			} else if (inputSplit.nextToken().equalsIgnoreCase("Jual")) {
 				String nama = inputSplit.nextToken();
 				farmer.sellAyam(nama);
-				System.out.println(farmer.getNama() + " menjual ayam bernama " + nama);
-				System.out.println("Uang " + farmer.getNama() + " sekarang: " + farmer.getMoney() + " G");
+				System.out.println(farmer.getNama() + " menjual ayam bernama "
+						+ nama);
+				System.out.println("Uang " + farmer.getNama() + " sekarang: "
+						+ farmer.getDuit() + " G");
 			} else if (inputSplit.nextToken().equalsIgnoreCase("Angkat")) {
 				String nama = inputSplit.nextToken();
 				farmer.pickUpAyam(nama);
-				System.out.println("Affection ayam " + nama + " bertambah 1 :)" );
+				System.out
+						.println("Affection ayam " + nama + " bertambah 1 :)");
 			} else if (inputSplit.nextToken().equalsIgnoreCase("Upgrade")) {
 				farmer.upgradeKandang();
-				System.out.println(farmer.getNama() + " mengupgrade kandang. Kapasitas baru : " + farmer.getKandang().size());
+				System.out.println(farmer.getNama()
+						+ " mengupgrade kandang. Kapasitas baru : "
+						+ farmer.getKandang().size());
 			} else if (inputSplit.nextToken().equalsIgnoreCase("Kick")) {
 				String nama = inputSplit.nextToken();
 				farmer.kickAyam(nama);
-				System.out.println("Affection ayam " + nama + " berkurang 1 :(" );
+				System.out
+						.println("Affection ayam " + nama + " berkurang 1 :(");
 			}
 		}
-		
-		
 	}
 }
