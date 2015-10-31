e710ef0e32d5f68174ec31472688b94661a77aa7
Mahdiazhari <mahdi.austian@gmail.com>
2015-10-31 20:36:20 +0700

    Update Lab06.java

--- Lab06.java
+++ Lab06.java
@@ -11,17 +11,24 @@ public class Lab06 {
 			if (inputSplit.nextToken().equalsIgnoreCase("Beli")) {
 				String nama = inputSplit.nextToken();
 				farmer.buyAyam(nama);
+				System.out.println(farmer.getNama() + " membeli ayam bernama " + nama);
+				System.out.println("Uang " + farmer.getNama() + " sekarang: " + farmer.getMoney() + " G");
 			} else if (inputSplit.nextToken().equalsIgnoreCase("Jual")) {
 				String nama = inputSplit.nextToken();
 				farmer.sellAyam(nama);
+				System.out.println(farmer.getNama() + " menjual ayam bernama " + nama);
+				System.out.println("Uang " + farmer.getNama() + " sekarang: " + farmer.getMoney() + " G");
 			} else if (inputSplit.nextToken().equalsIgnoreCase("Angkat")) {
 				String nama = inputSplit.nextToken();
 				farmer.pickUpAyam(nama);
+				System.out.println("Affection ayam " + nama + " bertambah 1 :)" );
 			} else if (inputSplit.nextToken().equalsIgnoreCase("Upgrade")) {
 				farmer.upgradeKandang();
+				System.out.println(farmer.getNama() + " mengupgrade kandang. Kapasitas baru : " + farmer.getKandang().size());
 			} else if (inputSplit.nextToken().equalsIgnoreCase("Kick")) {
 				String nama = inputSplit.nextToken();
 				farmer.kickAyam(nama);
+				System.out.println("Affection ayam " + nama + " berkurang 1 :(" );
 			}
 		}
 		
